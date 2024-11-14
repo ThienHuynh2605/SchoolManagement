153528905e30285fc751ade3648cda57be1f64f9
Thien.Huynh2 <Thien.Huynh2@sts.saigontechnology>
2024-11-14 09:06:38 +0700

    Initial with develop branch

--- SchoolManagement.Application/obj/Debug/net8.0/SchoolManagement.Application.AssemblyInfo.cs
+++ SchoolManagement.Application/obj/Debug/net8.0/SchoolManagement.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SchoolManagement.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+061a04a70731d35481dbfbb84b88dc140d7097ec")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7dc3f8931ff751ed3dcb6457c9f7fa3d2358c580")]
 [assembly: System.Reflection.AssemblyProductAttribute("SchoolManagement.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SchoolManagement.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
